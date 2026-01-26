2fcc48d10ac3542bf6ae5404858f0d9949653cd5
toni <lostmosquito@gmail.com>
2026-01-26 18:01:06 +0300

    fix: white screen issues (legacy support + base path + eruda)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import legacy from '@vitejs/plugin-legacy';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   build: {
     outDir: 'dist',
     // Важно: es2015 обеспечивает работу на старых Android (Telegram WebView)
